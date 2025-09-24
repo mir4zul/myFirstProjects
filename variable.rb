df69505bec15e2a3dad287b3f823e1987055a6ab
mir4zul <mirajulislam0509@gmail.com>
2025-09-24 12:13:34 +0600

    variable data type

--- variable.rb
+++ variable.rb
@@ -5,3 +5,6 @@ puts "There once was a man named #{character_name}."
 puts "He was #{character_age} years old."
 
 
+character_name = "Arslan"
+puts "He really liked the name #{character_name}.".upcase
+puts "But didn't like being #{character_age}.".downcase
