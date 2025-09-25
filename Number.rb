a58a58c35dded036661f1f4ccb0eaff342b55b36
mir4zul <mirajulislam0509@gmail.com>
2025-09-25 21:35:12 +0600

    variable data type

---
+++ Number.rb
@@ -0,0 +1,17 @@
+# Integer number
+IntNum = 10
+
+# floating point number
+FloNum = 10.5
+
+# Negative Number
+NegNum = -20
+
+# complex number
+ComNum = 3 + 4i
+
+# Big number
+BigNum = 1234567890123456789012345678901234567890
+
+# Scientific notation
+SciNum = 1.5e3  # 1.5 * 10^3 = 1500.0
