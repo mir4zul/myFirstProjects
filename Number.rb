29c882fa8b44e09d52e4066b5d0f13b0d440a766
mir4zul <mirajulislam0509@gmail.com>
2025-10-14 23:22:28 +0600

    variable data type

--- Number.rb
+++ Number.rb
@@ -19,20 +19,3 @@ SciNum = 1.5e3  # 1.5 * 10^3 = 1500.0
 # Underscore in number for readability
 # (available in Ruby 2.0 and later)
 ReadableNum = 1_000_000
-
-
-# Output the numbers
-puts "Integer Number: #{IntNum}"
-puts "Floating Point Number: #{FloNum}"
-puts "Negative Number: #{NegNum}"
-puts "Complex Number: #{ComNum}"
-puts "Big Number: #{BigNum}"
-puts "Scientific Notation: #{SciNum}"
-puts "Readable Number: #{ReadableNum}"
-puts "Type of IntNum: #{IntNum.class}"
-puts "Type of FloNum: #{FloNum.class}"
-puts "Type of NegNum: #{NegNum.class}"
-puts "Type of ComNum: #{ComNum.class}"
-puts "Type of BigNum: #{BigNum.class}"
-puts "Type of SciNum: #{SciNum.class}"
-puts "Type of ReadableNum: #{ReadableNum.class}"
