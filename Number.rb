7459f5b7c059cda7dd76eb4dedcfd0089c11c163
mir4zul <mirajulislam0509@gmail.com>
2025-10-15 12:41:27 +0600

    Number type for

--- Number.rb
+++ Number.rb
@@ -19,3 +19,19 @@ SciNum = 1.5e3  # 1.5 * 10^3 = 1500.0
 # Underscore in number for readability
 # (available in Ruby 2.0 and later)
 ReadableNum = 1_000_000
+
+Power = 2**3  # 2 raised to the power of 3
+remainder = 10 % 3  # Remainder of 10 divided by 3
+HexNum = 0x1A  # Hexadecimal representation (26 in decimal)
+OctNum = 0o32  # Octal representation (26 in decimal)
+BinNum = 0b11010  # Binary representation (26 in decimal)
+MaxInt = Float::INFINITY  # Represents an infinitely large number
+MinInt = -Float::INFINITY  # Represents an infinitely small (negative) number
+NotANumber = Float::NAN  # Represents a value that is "not a number
+
+puts "power: #{Power}"
+puts "remainder: #{remainder}"
+puts "Hexadecimal: #{HexNum}"
+puts "MaxInt: #{MaxInt}"
+puts "MinInt: #{MinInt}"
+puts "NotANumber: #{NotANumber}"
