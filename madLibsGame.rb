008de52274c6716be9c06d553a6ce08d686eca05
mir4zul <mirajulislam0509@gmail.com>
2025-10-20 01:01:00 +0600

    Add mad libs game with user input for color, plural noun, and celebrity

---
+++ madLibsGame.rb
@@ -0,0 +1,12 @@
+puts "Enter a color:"
+color = gets.chomp
+puts "Enter a plural noun:"
+plural_noun = gets.chomp
+puts "Enter a celebrity:"
+celebrity = gets.chomp
+
+
+puts "Roses are ___#{color}___,"
+puts "#{plural_noun} are blue,"
+puts "I love ___#{celebrity}___,"
+puts "And so do you."
