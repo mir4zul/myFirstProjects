ae0830f45354b495a426aedb7d86b0be0ec32c2c
mir4zul <mirajulislam0509@gmail.com>
2025-10-20 00:14:58 +0600

    Add basic calculator functionality with user input

---
+++ basicCalculator.rb
@@ -0,0 +1,26 @@
+puts "Enter a number: "
+num1 = gets.chomp.to_f
+puts "Enter another number: "
+num2 = gets.chomp.to_f
+
+puts "Choose an operation (+, -, *, /): "
+operation = gets.chomp
+
+result = case operation
+         when "+"
+           num1 + num2
+         when "-"
+           num1 - num2
+         when "*"
+           num1 * num2
+         when "/"
+           if num2 != 0
+             num1 / num2
+           else
+             "Error: Division by zero"
+           end
+         else
+           "Invalid operation"
+         end
+
+puts "The result is: #{result}"
