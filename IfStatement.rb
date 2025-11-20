4c89347350ea257d5dd687e79204048e830c13a3
mir4zul <mirajulislam0509@gmail.com>
2025-11-20 21:59:41 +0600

    Add if statment or

--- IfStatement.rb
+++ IfStatement.rb
@@ -9,4 +9,9 @@ else
   puts "You are not a male or not tall"
 end
 
+if ismale or istall # or is used for at least one condition true then true
+  puts "You are a" + (ismale ? " male" : "") + (istall ? " tall" : "")
+else
+  puts "You are not a male or not tall"
+end
 
