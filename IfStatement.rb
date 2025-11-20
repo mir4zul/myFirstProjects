b42a70f649297324f022635301aefc33e043b8f9
mir4zul <mirajulislam0509@gmail.com>
2025-11-20 21:59:25 +0600

    Add if statment

--- IfStatement.rb
+++ IfStatement.rb
@@ -1,2 +1,12 @@
-# frozen_string_literal: true
+ismale = true
+istall = false
+
+if ismale and istall # and is used for both conditions true then only true
+  puts "You are a male"
+elsif ismale
+  puts "You are a male but not tall"
+else
+  puts "You are not a male or not tall"
+end
+
 
