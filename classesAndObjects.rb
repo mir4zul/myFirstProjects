6ca3af08cd44c9eafa58640bb4e5e1d177900483
mir4zul <mirajulislam0509@gmail.com>
2026-01-27 11:19:31 +0600

    Classes and Objects

--- classesAndObjects.rb
+++ classesAndObjects.rb
@@ -1,3 +1,17 @@
 class Book
-  attr_accessor :title, :author, :page
+  attr_accessor :title, :author, :pages
 end
+
+book = Book.new
+book.title = "1984"
+book.author = "George Orwell"
+book.pages = 328
+
+book1 = Book.new
+book1.title = "The Great Gatsby"
+book1.author = "F. Scott Fitzgerald"
+book1.pages = 218
+
+puts book.title
+puts book1.title
+
